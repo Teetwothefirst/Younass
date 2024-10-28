a83811d5d506cf500e188306a54ecccf8ca982c3
Teetwothefirst <dexterdavid835@gmail.com>
2024-10-28 12:41:37 -0700

    Changes to the Landing Page

--- younass/src/pages/Landing.jsx
+++ younass/src/pages/Landing.jsx
@@ -74,23 +74,28 @@ function Landing(){
 
 
             <div>
-                <div className="md:flex justify-center px-10">
+                <div className="mb-20 md:flex justify-center px-10">
                     <div className="flex justify-between gap-5">
                         <div className="bgImg relative">
                                 <img src={cartImg3} alt="" className="w-full h-100" />
-                                <div className="absolute top-28 w-full h-full flex justify-center items-center">
-                                    <div className="text-white">
+                                <div className="absolute top-40 w-full h-full flex justify-center items-center">
+                                    <div className="text-black md:text-white">
                                     {/* <p className="text-center text-s md:text-xl">FALL/WINTER 2024</p> */}
                                     <h2 className="text-xl md:text-4xl">KIMONOS AND JACKETS</h2>
-                                    <div className="text-center">WOMEN <i className="bi bi-arrow-right"></i> CHILDREN <i className="bi bi-arrow-right"></i> MEN <i className="bi bi-arrow-right"></i></div>
+                                    <div className="sm:text-s md:text-center">WOMEN <i className="bi bi-arrow-right"></i> CHILDREN <i className="bi bi-arrow-right"></i> MEN <i className="bi bi-arrow-right"></i></div>
                                     </div>
-                                    
                                 </div>
+                                {/* <div className="sm:absolute top-0 md:hidden">
+                                <div className="text-white">
+                                    <h2 className="text-xl">KIMONOS AND JACKETS</h2>
+                                    <div className="text-s">WOMEN <i className="bi bi-arrow-right"></i> CHILDREN <i className="bi bi-arrow-right"></i> MEN <i className="bi bi-arrow-right"></i></div>
+                                    </div>
+                                </div> */}
                         </div>
                         <div className="bgImg relative">
                             <img src={cartImg4} alt="" className="w-full h-100" />
-                            <div className="absolute top-28 w-full h-full flex justify-center items-center">
-                                <div className="text-white">
+                            <div className="absolute top-40 w-full h-full flex justify-center items-center">
+                                <div className="text-black md:text-white">
                                 {/* <p className="text-center text-s md:text-xl">FALL/WINTER 2024</p> */}
                                 <h2 className="text-xl md:text-4xl">KAFTAN AND BUBUS</h2>
                                 <div className="text-center">WOMEN <i className="bi bi-arrow-right"></i> CHILDREN <i className="bi bi-arrow-right"></i> MEN <i className="bi bi-arrow-right"></i></div>
